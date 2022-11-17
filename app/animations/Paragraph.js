84a0e34447ecd62cf88f57105a32b0fae7702166
Marcos Sanchez <marcos.sanchez@saei.mx>
2022-11-17 11:11:25 -0600

    Start implementing animations for paragraphs. Animations to titles are done OK

--- app/animations/Paragraph.js
+++ app/animations/Paragraph.js
@@ -3,21 +3,14 @@ import Animation from "classes/Animation";
 import each from "lodash/each";
 import { calculate, split } from "utils/text";
 
-export default class Title extends Animation {
+export default class Paragraph extends Animation {
   constructor({ element, elements }) {
     super({ element, elements }); // el element que recibes en el constructor se los mandas al super
 
-    split({
-      element: this.element,
-      append: true,
-    });
-
-    split({
+    this.elementLinesSpans = split({
       element: this.element, // separa en spams cada palabra del titutlo
       append: true,
     });
-
-    this.elementLinesSpans = document.querySelectorAll("span span");
   }
 
   animateIn() {
@@ -28,9 +21,15 @@ export default class Title extends Animation {
     each(this.elementLines, (line, index) => {
       this.timelineIn.fromTo(
         line, //elemento a animar
-        { y: "100%" }, //objeto animacion FROM
+        { y: "100%", autoAlpha: 0 }, //objeto animacion FROM
         // stagger sirve para correr una nueva animacion despues de haber terminado la actual
-        { duration: 1.5, delay: index * 0.2, ease: "expo.out", y: "0%" }, // objeto animacion TO
+        {
+          autoAlpha: 1,
+          duration: 1.5,
+          delay: index * 0.2,
+          ease: "expo.out",
+          y: "0%",
+        }, // objeto animacion TO
         0
       );
     });
--- app/classes/Page.js
+++ app/classes/Page.js
@@ -94,6 +94,8 @@ export default class Page {
       this.scroll.limit =
         this.elements.wrapper.clientHeight - window.innerHeight;
     }
+
+    each(this.animationsTitles, (animation) => animation.onResize());
   }
 
   update() {
