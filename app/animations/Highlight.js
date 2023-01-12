55aece1e0e4f07b0a13640fd91df661b2a8163ed
Marcos Sanchez <msanchez@hircasa.com>
2023-01-11 22:36:12 -0600

    test commit for fastImmersiveSite

--- app/animations/Highlight.js
+++ app/animations/Highlight.js
@@ -1,26 +1,28 @@
 import GSAP from "gsap";
 import Animation from "classes/Animation";
-import each from "lodash/each";
-import { calculate, split } from "utils/text";
 
 export default class Highlight extends Animation {
   constructor({ element, elements }) {
-    super({ element, elements }); // el element que recibes en el constructor se los mandas al super
+    super({ element, elements });
   }
 
   animateIn() {
-    this.timelineIn = GSAP.timeline({ delay: 0.5 });
-
-    this.timelineIn.fromTo(
+    GSAP.fromTo(
       this.element,
-      { autoAlpha: 0, scale: 1.2 },
-      { autoAlpha: 1, duration: 1.5, scale: 1, ease: "expo.out" }
+      {
+        autoAlpha: 0,
+        delay: 0.5,
+      },
+      {
+        autoAlpha: 1,
+        duration: 1,
+      }
     );
   }
 
   animateOut() {
-    GSAP.set(this.element, { autoAlpha: 0 });
+    GSAP.set(this.element, {
+      autoAlpha: 0,
+    });
   }
-
-  onResize() {}
 }
